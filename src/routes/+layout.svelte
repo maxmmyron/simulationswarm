c6de96199f380f14215f4bc2f3cd77edbf3ce424
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-10-20 15:11:22 -0400

    delegate content to +page, implement submit front-end

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,3 @@
-<script>
-  import data from "$lib/data.json";
-</script>
-
 <header>
 	<a href="/">simulationswarm</a>
 	<form action="?/search" method="POST">
@@ -28,40 +24,4 @@
   <a href="/submit">Submit</a>
 </header>
 
-<main>
-  <h1>simulationswarm is a curated index of asset collections and design reference collages. stop adding new collections to a word doc—add them here instead.</h1>
-
-  {#each data as listing}
-    <article>
-      <img src="/img/{listing.preview}" alt={listing.name} />
-      <h2>
-        <a href={listing.href} target="_blank">{listing.name}</a>
-      </h2>
-      <p>{listing.desc}</p>
-      <div>
-        <p>{listing.color}</p>
-        <p>{listing.type}</p>
-      </div>
-
-    </article>
-  {/each}
-</main>
-
-<style>
-  article {
-    border: 1px solid black;
-    padding: 1rem;
-    margin: 1rem 0;
-    max-width: 500px;
-  }
-
-  article > img {
-    width: 100%;
-  }
-
-  article > div {
-    display: flex;
-    justify-content: space-between;
-    gap: 1rem;
-  }
-</style>
+<slot />
---
+++ src/routes/submit/+page.svelte
@@ -0,0 +1,10 @@
+<main>
+  <h1>Submit a collection</h1>
+
+  <form action="?/submit">
+    <input type="text" name="url" placeholder="URL" />
+    <input type="text" name="title" placeholder="Title" />
+
+    <button type="submit">Submit</button>
+  </form>
+</main>
